f07ff2fc919bd4c5a0ceba2eb18b3917d47499e8
Giuseppe Manzo <manzo.giuseppe@gmail.com>
2020-10-16 13:49:56 +0000

    controllo ora e null value

--- main.php
+++ main.php
@@ -5,12 +5,16 @@ function remove_value(&$array, $value) {
   if($i) array_splice($array, $i, 1);
 }
 
+function add_unique_value(&$array, $value) {
+  $i = array_search($value, $array, true);
+  if( !$i ) $array[] = $value;
+}
 
 $CHECK_ORA = '';
 $Orari = array();
 $Servizi = array();
 
-$rimanenti = array();
+$valorizzati = array();
 $dafare = array();
 
 $csv = file("data_copy.txt");
@@ -23,21 +27,34 @@ foreach( $csv as $key=>$value) {
   $ora = $line[0];
   $nome_servizio = $line[1];
   $valore = intval($line[2]);
+  
+  if( $ora !== $CHECK_ORA ) {
+    $CHECK_ORA = $ora;
+    $Orari[] = $ora;
+    $nonvalorizzati = array_diff( array_keys($Servizi), $valorizzati );
+
+    foreach($nonvalorizzati as $n) {
+      $Servizi[$n][] = '-';
+    }
+
+    $valorizzati = array();
+  }
 
   if( !array_key_exists($nome_servizio, $Servizi) ) {
     $Servizi[$nome_servizio] = array_fill(0, count($Orari), '-');
   }
-
   array_push( $Servizi[$nome_servizio], $valore );
+  add_unique_value($valorizzati, $nome_servizio);
 
   /*if( $valore >= 30 && !in_array($nome_servizio, $dafare) ) {
     $dafare[] = $nome_servizio;
   }*/
 
-  
-  if( $ora !== $CHECK_ORA ) {
-    $CHECK_ORA = $ora;
-    $Orari[] = $ora;
-  }
 }
+
+$nonvalorizzati = array_diff( array_keys($Servizi), $valorizzati );
+foreach($nonvalorizzati as $n) {
+  $Servizi[$n][] = '-';
+}
+
 print_r($Servizi);
